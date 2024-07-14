d7571d3f71a95448207c62681e0c7e38bdeae40e
irakiza44 <irakiza44@gmail.com>
2024-07-14 20:32:48 +0200

    Fixing HttpClient warnings

--- src/app/components/app.module.ts
+++ src/app/components/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import {
+  HttpClientModule,
+  provideHttpClient,
+  withFetch,
+} from '@angular/common/http';
 
 import { AppRoutingModule } from '../app-routing.module';
 import { AppComponent } from '../app.component';
@@ -22,6 +26,7 @@ import { CategorySportsComponent } from './category-sports/category-sports.compo
     CategorySportsComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [provideHttpClient(withFetch())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
