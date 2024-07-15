4140a7fb049d5b3f2f145b072195117c09618da8
irakiza44 <irakiza44@gmail.com>
2024-07-15 15:14:13 +0200

    designing with responsiviness to start screen and how to play pages

--- src/app/components/start-screen/start-screen.component.ts
+++ src/app/components/start-screen/start-screen.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-start-screen',
   templateUrl: './start-screen.component.html',
-  styleUrl: './start-screen.component.css'
+  styleUrl: './start-screen.component.css',
 })
 export class StartScreenComponent {
+  constructor(private router: Router) {}
 
+  startGame() {
+    this.router.navigate(['/pick-category']);
+  }
 }
